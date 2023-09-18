86f32bb814bc5eb88df5a9c3bb24c0e964b55ab7
Muhammad Shahzaib <shahzaibmuhammad627@gmail.com>
2023-09-18 11:35:21 +0500

    Slider launched

--- src/Components/Film.js
+++ src/Components/Film.js
@@ -1,8 +1,17 @@
 import React, { useEffect, useState } from "react";
+import Slider from "react-slick";
 
 const Film = () => {
   const [moviesData, setMoviesData] = useState(null); // Initialize state to store fetched data
-
+  var settings = {
+    dots: true,
+    pauseOnHover: true,
+    infinite: true,
+    speed: 3000,
+    autoplay: true,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+  };
   async function fetchMovies() {
     const api = process.env.API_KEY;
     const apiUrl = `https://api.themoviedb.org/3/discover/movie?api_key=0a4ea0f6b58dd38f569836183f3dbf13`;
@@ -60,51 +69,55 @@ const Film = () => {
               <div className="carousel-inner">
                 <div className="carousel-item active">
                   <div className="trend_2i row">
-                    {moviesData ? (
-                      moviesData.results.map((movie) => (
-                        // Render each movie from the API
-                        <div
-                          className="col-md-3 col-6 my-4"
-                          key={movie.id}
-                          id={movie.id}
-                        >
-                          <div className="trend_2im clearfix position-relative">
-                            <div className="trend_2im1 clearfix">
-                              <div className="grid">
-                                <figure className="effect-jazz mb-0">
-                                  <a href="#">
-                                    <img
-                                      src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
-                                      className="w-100"
-                                      alt={movie.title}
-                                    />
-                                  </a>
-                                </figure>
+                    <Slider {...settings}>
+                      {moviesData ? (
+                        moviesData.results.map((movie) => (
+                          // Render each movie from the API
+                          <div
+                            className="col-md-12 col-6 px-3 my-4"
+                            key={movie.id}
+                            id={movie.id}
+                          >
+                            <div className="trend_2im clearfix position-relative">
+                              <div className="trend_2im1 clearfix">
+                                <div className="grid">
+                                  <figure className="effect-jazz mb-0">
+                                    <a href="#">
+                                      <img
+                                        src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+                                        className="w-100"
+                                        alt={movie.title}
+                                      />
+                                    </a>
+                                  </figure>
+                                </div>
                               </div>
                             </div>
+                            <div className="trend_2ilast bg_grey p-3 clearfix">
+                              <h5 className="title">
+                                <a className="col_red " href="#">
+                                  {movie.title}
+                                </a>
+                              </h5>
+                              <p className="mb-2 dotted-para">
+                                {movie.overview}
+                              </p>
+                              <span className="col_red">
+                                <i className="fa fa-star"></i>
+                                <i className="fa fa-star"></i>
+                                <i className="fa fa-star"></i>
+                                <i className="fa fa-star"></i>
+                                <i className="fa fa-star"></i>
+                              </span>
+                              <p className="mb-0">{movie.popularity} Views</p>
+                            </div>
                           </div>
-                          <div className="trend_2ilast bg_grey p-3 clearfix">
-                            <h5 className="title">
-                              <a className="col_red " href="#">
-                                {movie.title}
-                              </a>
-                            </h5>
-                            <p className="mb-2 dotted-para">{movie.overview}</p>
-                            <span className="col_red">
-                              <i className="fa fa-star"></i>
-                              <i className="fa fa-star"></i>
-                              <i className="fa fa-star"></i>
-                              <i className="fa fa-star"></i>
-                              <i className="fa fa-star"></i>
-                            </span>
-                            <p className="mb-0">{movie.popularity} Views</p>
-                          </div>
-                        </div>
-                      ))
-                    ) : (
-                      // Render a loading message or spinner while data is being fetched
-                      <div>Loading...</div>
-                    )}
+                        ))
+                      ) : (
+                        // Render a loading message or spinner while data is being fetched
+                        <div>Loading...</div>
+                      )}
+                    </Slider>
                   </div>
                 </div>
               </div>
--- src/Components/Tv.js
+++ src/Components/Tv.js
@@ -1,8 +1,17 @@
 import React, { useEffect, useState } from "react";
+import Slider from "react-slick";
 
 const Tv = () => {
   const [moviesData, setMoviesData] = useState(null); // Initialize state to store fetched data
-
+  var settings = {
+    dots: true,
+    pauseOnHover: true,
+    infinite: true,
+    speed: 3000,
+    autoplay: true,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+  };
   async function fetchMovies() {
     const api = process.env.API_KEY;
     const apiUrl = `https://api.themoviedb.org/3/discover/tv?api_key=0a4ea0f6b58dd38f569836183f3dbf13`;
@@ -35,7 +44,7 @@ const Tv = () => {
               <div className="trend_1l">
                 <h4 className="mb-0">
                   <i className="fa fa-youtube-play align-middle col_red me-1"></i>{" "}
-                  Latest <span className="col_red">Movies</span>
+                  Latest <span className="col_red">Tv Series</span>
                 </h4>
               </div>
             </div>
@@ -57,59 +66,58 @@ const Tv = () => {
               className="carousel slide"
               data-bs-ride="carousel"
             >
-              <div className="carousel-inner">
-                <div className="carousel-item active">
-                  <div className="trend_2i row">
-                    {moviesData ? (
-                      moviesData.results.map((movie) => (
-                        // Render each movie from the API
-                        <div className="col-md-3 col-6 my-4" key={movie.id}>
-                          <div className="trend_2im clearfix position-relative">
-                            <div className="trend_2im1 clearfix">
-                              <div className="grid">
-                                <figure className="effect-jazz mb-0">
-                                  <a href="#">
-                                    <img
-                                      src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
-                                      className="w-100"
-                                      alt={movie.title}
-                                    />
-                                  </a>
-                                </figure>
-                              </div>
+              <div className="trend_2i row">
+                <Slider {...settings}>
+                  {moviesData ? (
+                    moviesData.results.map((movie) => (
+                      // Render each movie from the API
+
+                      <div className="col-md-12 col-6 px-3 my-4" key={movie.id}>
+                        <div className="trend_2im clearfix position-relative">
+                          <div className="trend_2im1 clearfix">
+                            <div className="grid">
+                              <figure className="effect-jazz mb-0">
+                                <a href="#">
+                                  <img
+                                    src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+                                    className="w-100"
+                                    alt={movie.title}
+                                  />
+                                </a>
+                              </figure>
                             </div>
                           </div>
-                          <div className="trend_2ilast bg_grey p-3 clearfix">
-                            <h5 className="title">
-                              <a className="col_red " href="#">
-                                {movie.name}
-                              </a>
-                            </h5>
-                            <p className="mb-2 dotted-para">
-                              {" "}
-                              {!movie.overview ? (
-                                <p>we are updating...</p>
-                              ) : (
-                                movie.overview
-                              )}
-                            </p>
-                            <span className="col_red">
-                              <i className="fa fa-star"></i>
-                              <i className="fa fa-star"></i>
-                              <i className="fa fa-star"></i>
-                              <i className="fa fa-star"></i>
-                              <i className="fa fa-star"></i>
-                            </span>
-                            <p className="mb-0">{movie.popularity} Views</p>
-                          </div>
                         </div>
-                      ))
-                    ) : (
-                      // Render a loading message or spinner while data is being fetched
-                      <div>Loading...</div>
-                    )}
-                  </div>
-                </div>
+                        <div className="trend_2ilast bg_grey p-3 clearfix">
+                          <h5 className="title">
+                            <a className="col_red " href="#">
+                              {movie.name}
+                            </a>
+                          </h5>
+                          <p className="mb-2 dotted-para">
+                            {" "}
+                            {!movie.overview ? (
+                              <p>we are updating...</p>
+                            ) : (
+                              movie.overview
+                            )}
+                          </p>
+                          <span className="col_red">
+                            <i className="fa fa-star"></i>
+                            <i className="fa fa-star"></i>
+                            <i className="fa fa-star"></i>
+                            <i className="fa fa-star"></i>
+                            <i className="fa fa-star"></i>
+                          </span>
+                          <p className="mb-0">{movie.popularity} Views</p>
+                        </div>
+                      </div>
+                    ))
+                  ) : (
+                    // Render a loading message or spinner while data is being fetched
+                    <div>Loading...</div>
+                  )}
+                </Slider>
               </div>
             </div>
           </div>
