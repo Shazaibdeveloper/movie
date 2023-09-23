5c185d47bd8f3fd9e56d2eccbc2d967f346f5584
Muhammad Shahzaib <shahzaibmuhammad627@gmail.com>
2023-09-23 09:59:31 +0500

    steam componenet added

---
+++ src/Components/Stream.js
@@ -0,0 +1,389 @@
+import React from "react";
+
+const Stream = () => {
+  return (
+    <>
+      <section id="stream" class="pb-5 pt-4">
+        <div class="container">
+          <div class="row trend_1">
+            <div class="col-md-6 col-6">
+              <div class="trend_1l">
+                <h4 class="mb-0">
+                  <i class="fa fa-youtube-play align-middle col_red me-1"></i>{" "}
+                  Movie <span class="col_red">Streaming Services</span>
+                </h4>
+              </div>
+            </div>
+            <div class="col-md-6 col-6">
+              <div class="trend_1r text-end">
+                <h6 class="mb-0">
+                  <a class="button" href="#">
+                    {" "}
+                    View All
+                  </a>
+                </h6>
+              </div>
+            </div>
+          </div>
+          <div class="row trend_2 mt-4">
+            <div
+              id="carouselExampleCaptions4"
+              class="carousel slide"
+              data-bs-ride="carousel"
+            >
+              <div class="carousel-indicators">
+                <button
+                  type="button"
+                  data-bs-target="#carouselExampleCaptions4"
+                  data-bs-slide-to="0"
+                  class="active"
+                  aria-label="Slide 1"
+                  aria-current="true"
+                ></button>
+                <button
+                  type="button"
+                  data-bs-target="#carouselExampleCaptions4"
+                  data-bs-slide-to="1"
+                  aria-label="Slide 2"
+                  class=""
+                ></button>
+              </div>
+              <div class="carousel-inner">
+                <div class="carousel-item active">
+                  <div class="trend_2i row">
+                    <div class="col">
+                      <div class="trend_2im clearfix position-relative">
+                        <div class="trend_2im1 clearfix">
+                          <div class="grid">
+                            <figure class="effect-jazz mb-0">
+                              <a href="#">
+                                <img
+                                  src="img/4.jpg"
+                                  class="w-100"
+                                  alt="img25"
+                                />
+                              </a>
+                            </figure>
+                          </div>
+                        </div>
+                        <div class="trend_2im2 clearfix  position-absolute w-100 top-0">
+                          <h5>
+                            <a class="col_red" href="#">
+                              Semper
+                            </a>
+                          </h5>
+                          <span class="col_red">
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                          </span>
+                          <p class="mb-0">2 Views</p>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="col">
+                      <div class="trend_2im clearfix position-relative">
+                        <div class="trend_2im1 clearfix">
+                          <div class="grid">
+                            <figure class="effect-jazz mb-0">
+                              <a href="#">
+                                <img
+                                  src="img/5.jpg"
+                                  class="w-100"
+                                  alt="img25"
+                                />
+                              </a>
+                            </figure>
+                          </div>
+                        </div>
+                        <div class="trend_2im2 clearfix  position-absolute w-100 top-0">
+                          <h5>
+                            <a class="col_red" href="#">
+                              Semper
+                            </a>
+                          </h5>
+                          <span class="col_red">
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                          </span>
+                          <p class="mb-0">2 Views</p>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="col">
+                      <div class="trend_2im clearfix position-relative">
+                        <div class="trend_2im1 clearfix">
+                          <div class="grid">
+                            <figure class="effect-jazz mb-0">
+                              <a href="#">
+                                <img
+                                  src="img/6.jpg"
+                                  class="w-100"
+                                  alt="img25"
+                                />
+                              </a>
+                            </figure>
+                          </div>
+                        </div>
+                        <div class="trend_2im2 clearfix  position-absolute w-100 top-0">
+                          <h5>
+                            <a class="col_red" href="#">
+                              Semper
+                            </a>
+                          </h5>
+                          <span class="col_red">
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                          </span>
+                          <p class="mb-0">2 Views</p>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="col">
+                      <div class="trend_2im clearfix position-relative">
+                        <div class="trend_2im1 clearfix">
+                          <div class="grid">
+                            <figure class="effect-jazz mb-0">
+                              <a href="#">
+                                <img
+                                  src="img/7.jpg"
+                                  class="w-100"
+                                  alt="img25"
+                                />
+                              </a>
+                            </figure>
+                          </div>
+                        </div>
+                        <div class="trend_2im2 clearfix  position-absolute w-100 top-0">
+                          <h5>
+                            <a class="col_red" href="#">
+                              Semper
+                            </a>
+                          </h5>
+                          <span class="col_red">
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                          </span>
+                          <p class="mb-0">2 Views</p>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="col">
+                      <div class="trend_2im clearfix position-relative">
+                        <div class="trend_2im1 clearfix">
+                          <div class="grid">
+                            <figure class="effect-jazz mb-0">
+                              <a href="#">
+                                <img
+                                  src="img/8.jpg"
+                                  class="w-100"
+                                  alt="img25"
+                                />
+                              </a>
+                            </figure>
+                          </div>
+                        </div>
+                        <div class="trend_2im2 clearfix  position-absolute w-100 top-0">
+                          <h5>
+                            <a class="col_red" href="#">
+                              Semper
+                            </a>
+                          </h5>
+                          <span class="col_red">
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                          </span>
+                          <p class="mb-0">2 Views</p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="carousel-item">
+                  <div class="trend_2i row">
+                    <div class="col">
+                      <div class="trend_2im clearfix position-relative">
+                        <div class="trend_2im1 clearfix">
+                          <div class="grid">
+                            <figure class="effect-jazz mb-0">
+                              <a href="#">
+                                <img
+                                  src="img/9.jpg"
+                                  class="w-100"
+                                  alt="img25"
+                                />
+                              </a>
+                            </figure>
+                          </div>
+                        </div>
+                        <div class="trend_2im2 clearfix  position-absolute w-100 top-0">
+                          <h5>
+                            <a class="col_red" href="#">
+                              Semper
+                            </a>
+                          </h5>
+                          <span class="col_red">
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                          </span>
+                          <p class="mb-0">2 Views</p>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="col">
+                      <div class="trend_2im clearfix position-relative">
+                        <div class="trend_2im1 clearfix">
+                          <div class="grid">
+                            <figure class="effect-jazz mb-0">
+                              <a href="#">
+                                <img
+                                  src="img/10.jpg"
+                                  class="w-100"
+                                  alt="img25"
+                                />
+                              </a>
+                            </figure>
+                          </div>
+                        </div>
+                        <div class="trend_2im2 clearfix  position-absolute w-100 top-0">
+                          <h5>
+                            <a class="col_red" href="#">
+                              Semper
+                            </a>
+                          </h5>
+                          <span class="col_red">
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                          </span>
+                          <p class="mb-0">2 Views</p>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="col">
+                      <div class="trend_2im clearfix position-relative">
+                        <div class="trend_2im1 clearfix">
+                          <div class="grid">
+                            <figure class="effect-jazz mb-0">
+                              <a href="#">
+                                <img
+                                  src="img/11.jpg"
+                                  class="w-100"
+                                  alt="img25"
+                                />
+                              </a>
+                            </figure>
+                          </div>
+                        </div>
+                        <div class="trend_2im2 clearfix  position-absolute w-100 top-0">
+                          <h5>
+                            <a class="col_red" href="#">
+                              Semper
+                            </a>
+                          </h5>
+                          <span class="col_red">
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                          </span>
+                          <p class="mb-0">2 Views</p>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="col">
+                      <div class="trend_2im clearfix position-relative">
+                        <div class="trend_2im1 clearfix">
+                          <div class="grid">
+                            <figure class="effect-jazz mb-0">
+                              <a href="#">
+                                <img
+                                  src="img/4.jpg"
+                                  class="w-100"
+                                  alt="img25"
+                                />
+                              </a>
+                            </figure>
+                          </div>
+                        </div>
+                        <div class="trend_2im2 clearfix  position-absolute w-100 top-0">
+                          <h5>
+                            <a class="col_red" href="#">
+                              Semper
+                            </a>
+                          </h5>
+                          <span class="col_red">
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                          </span>
+                          <p class="mb-0">2 Views</p>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="col">
+                      <div class="trend_2im clearfix position-relative">
+                        <div class="trend_2im1 clearfix">
+                          <div class="grid">
+                            <figure class="effect-jazz mb-0">
+                              <a href="#">
+                                <img
+                                  src="img/5.jpg"
+                                  class="w-100"
+                                  alt="img25"
+                                />
+                              </a>
+                            </figure>
+                          </div>
+                        </div>
+                        <div class="trend_2im2 clearfix  position-absolute w-100 top-0">
+                          <h5>
+                            <a class="col_red" href="#">
+                              Semper
+                            </a>
+                          </h5>
+                          <span class="col_red">
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                            <i class="fa fa-star"></i>
+                          </span>
+                          <p class="mb-0">2 Views</p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Stream;
